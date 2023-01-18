8dc426bc4a07f8fafaf6a93b994ecd9a1143e0f9
mirel.apetrei <mirel_apetrei@icloud.com>
2023-01-18 15:37:59 +0100

    aufgabe 9 verbesserung

--- week5/Tag23_Projekt/Aufgabe10/src/Ratespiel.kt
+++ week5/Tag23_Projekt/Aufgabe10/src/Ratespiel.kt
@@ -1,16 +1,23 @@
 import kotlin.random.Random
 
-fun main() {
     val computerGuess = Random.nextInt(1, 100)
+fun main() {
 
+   game()
+
+}
+
+fun game(){
     println("Please enter your number: ")
     var yourNumber = readln().toInt()
 
     do {
         if (yourNumber < computerGuess){
             println("Zu klein! ")
+            game()
         } else if (yourNumber > computerGuess){
             println("Zu groß")
+            game()
         } else {
             println("Du hast gewonnen!")
         }
