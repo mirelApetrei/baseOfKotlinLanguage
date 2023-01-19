50fd632942b25d14fc60c43f2f24939acdaee5d0
mirel.apetrei <mirel_apetrei@icloud.com>
2023-01-19 16:23:22 +0100

    aufgabe tag24 fertig/verbesserungen

--- week5/Tag24_Projekt/Aufgabe7/src/Jahreszeiten.kt
+++ week5/Tag24_Projekt/Aufgabe7/src/Jahreszeiten.kt
@@ -14,32 +14,37 @@ fun main() {
     )
 
 //    TODO: a)
-    //println(findSeason(seasons, "September"))
+    println("Aufgabe a): ")
+    println(findSeason(seasons, "September"))
 
 //    TODO: b) Schreibe jetzt in der main()-Funktion eine Schleife, die alle Monate mit ihrer Jahreszeit in der Konsole ausgibt.
-   /* for ((key, value) in seasons) {
+    println("\nAufgabe b): ")
+    for ((key, value) in seasons) {
         for (entry in value) {
             println("Der $entry ist im $key")
         }
-    }*/
+    }
 
 //    TODO: c) Schreibe zusätzlich eine Schleife deiner Wahl, die nur die Sommermonate in der Konsole ausgibt.
-    /*for ((key, value) in seasons){
+    println("\nAufgabe c): ")
+    for ((key, value) in seasons){
         if (key == "Sommer"){
             println(value)
         }
-    }*/
+    }
 
 //    TODO: d) Schreibe zusätzlich eine Schleife deiner Wahl, die alle Monate außer den Juni und den September ausgibt.
-    /*for (month in monate){
+    println("\nAufgabe d): ")
+    for (month in monate){
         if (month == "Juni" || month == "September") {
             continue
         }
         println(month)
-    }*/
+    }
 
 //    TODO: e) Schreibe zusätzlich eine Schleife deiner Wahl, die ab dem März jeden zweiten Monat ausgibt.
-    for (i in monate.indexOf("März") + 1..monate.size - 1){
+    println("\nAufgabe e): ")
+    for (i in monate.indexOf("März") ..monate.size - 1 step 2){
         println(monate[i])
     }
 
