bfbdd379d03bdf7925a7f0fd4d2cb7e6d8b605c9
mirel.apetrei <mirel_apetrei@icloud.com>
2023-01-25 11:26:30 +0100

    Tag 28 - Aufgaben

---
+++ week6/Tag28_Projekt/Aufgabe1/src/TextAbgabe.kt
@@ -0,0 +1,5 @@
+/*
+* erstellt eine Klasse Car, die zwei Eigenschaften(color: String, speed:Int) und zwei Methoden[paint(), tune()] hat, und eine constructor
+*
+* Im nächsten Codeblock werden die Methoden paint() und tune() -> aufgerufen, um die Farbe des Autos zu ändern und die Geschwindigkeit um 10 km/h zu erhöhen
+* */
