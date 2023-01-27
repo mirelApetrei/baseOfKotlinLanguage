2c373c96c7831d04774d19aad269cad9d62c0935
mirel.apetrei <mirel_apetrei@icloud.com>
2023-01-27 11:00:21 +0100

    Tag30 - PettingZoo_Projekt

--- week6/Tag29_Projekt/Aufgabe5/src/main5.kt
+++ week6/Tag29_Projekt/Aufgabe5/src/main5.kt
@@ -12,7 +12,7 @@ fun main(){
 //    TODO: b) Füge diese Zeile Code in die makeNoise()-Methode der Hund und der Katzen-Klasse hinzu: super.makeNoise()
     /*Animal noises...
     Woof!
-    Animal noises...   ==> ueber konsole wird printed
+    Animal noises...   ==> Über konsole wird printed
     Miau.*/
 
 }
